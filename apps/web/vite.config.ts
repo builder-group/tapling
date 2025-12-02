bd597c7893b9d7c0b532f420a3369063ece98eb2
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-12-02 07:59:47 +0100

    #develop init web

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+	server: {
+		port: 3000
+	},
+	plugins: [
+		tsConfigPaths({
+			projects: ['./tsconfig.json']
+		}),
+		tanstackStart({
+			srcDirectory: 'src'
+		}),
+		viteReact(),
+		tailwindcss()
+	]
+}));
